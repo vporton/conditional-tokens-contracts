2f75e718f900cd9f041017fb7cac463fc824d555
Victor Porton <porton@narod.ru>
2020-12-09 00:24:31 +0200

    contract renamed

--- contracts/BidOnAddressesIdent.sol
+++ contracts/BidOnAddressesIdent.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.1;
 import "./BidOnAddresses.sol";
 
 /// BidOnAddresses with BrightID identity
-contract ConditionalTokensManyIdent is BidOnAddresses {
+contract BidOnAddressesIdent is BidOnAddresses {
     // TODO: Nontranferrable token with its rent given to alive person tradeable and redeemable.
     // TODO: Also combine both variants.
     // TODO: Account recovery.
