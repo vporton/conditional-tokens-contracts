cacf807a5884dd4b926d0c88794660556a10a176
Victor Porton <porton@narod.ru>
2020-12-09 20:17:46 +0200

    links

--- contracts/BidOnAddressesIdent.sol
+++ contracts/BidOnAddressesIdent.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.5.1;
 import "./BidOnAddresses.sol";
 
+// https://brightid.gitbook.io
+// https://docs.google.com/document/d/16-gqjfJLz7WSYVDHVQSFKK3Mj0GFWetxdgE9nQuMS9o/edit#heading=h.s5d6g1yzfjl0
+
 /// BidOnAddresses with BrightID identity
 contract BidOnAddressesIdent is BidOnAddresses {
     // TODO: Nontranferrable token with its rent given to alive person tradeable and redeemable.
