267e861e546587fbb87f1b28558af7e940a80401
Victor Porton <porton@narod.ru>
2020-12-12 08:21:36 +0200

    allocate fund to the DAO itself

--- contracts/SalaryWithDAO.sol
+++ contracts/SalaryWithDAO.sol
@@ -7,7 +7,11 @@ interface OurDAO {
 }
 
 contract SalaryWithDAO is BaseRestorableSalary {
-    OurDAO dao;
+    using ABDKMath64x64 for int128;
+
+    OurDAO public dao;
+
+    int128 public daoShare = int128(0).div(1); // zero by default
 
     constructor(OurDAO _dao, string memory uri_) BaseRestorableSalary(uri_) {
         dao = _dao;
@@ -17,9 +21,22 @@ contract SalaryWithDAO is BaseRestorableSalary {
         dao = _dao;
     }
 
+    /// Set the multiplier of tokens given to the DAO
+    /// @param share is an 64x64 fraction. We don't check if it is above zero, because `.mulu()` will just fail in this case.
+    function setDaoShare(int128 share) public onlyDAO {
+        daoShare = share;
+    }
+
     /// Set the token URI.
     function setURI(string memory newuri) public onlyDAO {
         _setURI(newuri);
+    }
+
+    function _mintToCustomer(uint256 conditionalTokenId, uint256 amount, bytes calldata data) internal virtual override {
+        super._mintToCustomer(conditionalTokenId, amount, data);
+        if (daoShare != int128(0).div(1)) { // Save gas.
+            _mint(address(dao), conditionalTokenId, daoShare.mulu(amount), data);
+        }
     }
 
     function checkAllowedRestoreAccount(address oldAccount_, address newAccount_) public virtual override {
